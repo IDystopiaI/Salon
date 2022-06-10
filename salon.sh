808b90f03d8ba91337678aa6cd69af8ab17a97d2
IDystopiaI <dylan.london94@gmail.com>
2022-06-10 22:38:07 +0000

    Basic framework for salon.sh

--- salon.sh
+++ salon.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+echo -e "\n~~~~ Welcome to My Salon ~~~~\n"
+
+# Main menu fucntion
+  # case statement "what can I do for you" (serice options)
+  # cut
+  # trim
+  # dye
+  # style
+
+  # handle unsupported input(wrong type of input or out of range)
+
+
+# ask for phone number
+  # fetch client id using phone number
+  # if does not exist in customers table
+  # ask for name
+  # insert phone number and name
+
+  # handle bad input?
+
+
+# create booking
+  # ask what time they would like their $SERVICE
+  # echo I have you booked for $SERVICE at $TIME, $CNAME
