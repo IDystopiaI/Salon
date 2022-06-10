74c7818daea79b84bce1ab61c5615b8b605c8f3e
IDystopiaI <dylan.london94@gmail.com>
2022-06-10 23:38:06 +0000

    early salon.sh changes

--- salon.sh
+++ salon.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
 echo -e "\n~~~~ Welcome to My Salon ~~~~\n"
 
 # Main menu fucntion
   # case statement "what can I do for you" (serice options)
-  # cut
-  # trim
-  # dye
-  # style
+SERVICE_MENU() {
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+  echo -e "Hello, how can I help you?"
+  echo -e "\n1) Cut\n2) Dye\n3) Style \n4) Trim"
+  read MENU_RESPONSE
 
-  # handle unsupported input(wrong type of input or out of range)
+
+  SERVICE_RESULT=$($PSQL "SELECT * FROM services WHERE service_id = '$MENU_RESPONSE'")
+  echo $SERVICE_RESULT
+
+# check value of $SERVICE_RESPONSE
+if [[ -z $SERVICE_RESULT ]]
+then
+  SERVICE_MENU "Sorry, we do not offer that here.\nPlease select a number that corresponds \nto a service you would like to book.\n"
+else
+  echo $SERVICE_RESPONSE
+fi
+}
+
+# handle unsupported input(wrong type of input or out of range)
+
 
 
 # ask for phone number
@@ -24,3 +44,5 @@ echo -e "\n~~~~ Welcome to My Salon ~~~~\n"
 # create booking
   # ask what time they would like their $SERVICE
   # echo I have you booked for $SERVICE at $TIME, $CNAME
+
+  SERVICE_MENU
