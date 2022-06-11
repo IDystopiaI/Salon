8567c8a8ec2f2f57424d8627023a151d6b893b49
IDystopiaI <dylan.london94@gmail.com>
2022-06-11 23:23:27 +0000

    mostly working
    need to rename variables to match test validation?

--- salon.sh
+++ salon.sh
@@ -17,46 +17,53 @@ SERVICE_MENU() {
   read SERVICE_ID
 
   # Looks up input for service id and assigns it matching service_id from services
+  
   SERVICE_ID_SELECTED=$($PSQL "SELECT service_id FROM services where service_id = '$SERVICE_ID'")
-  echo $SERVICE_ID_SELECTED
+  # echo $SERVICE_ID_SELECTED
+  # fetch service name and strip white space
+  SERVICE_NAME=$($PSQL "SELECT name from services WHERE service_id = '$SERVICE_ID'" | sed -E 's/^ *| *$//g')
 
   # if invalid input
     if [[ -z $SERVICE_ID_SELECTED ]]
     then
       # send back to service menu with message
-      SERVICE_MENU "Sorry, we do not offer that here.\n Please select a number that corresponds to a service you would like to book.\n"
+      SERVICE_MENU "Sorry, we do not offer that here.\nPlease select a number that corresponds to a service you would like to book.\n"
 
     else
-    # SERVICE_ID_SELECTED exists
-    # ask for phone number to look up customer
-    echo "What is your phone number?"
-    read RECEIVED_PHONE
+      # SERVICE_ID_SELECTED exists
+      # ask for phone number to look up customer
+      echo -e "\nWhat is your phone number?"
+      read RECEIVED_PHONE
 
-    # see if number exists
-    CUSTOMER_PHONE=$($PSQL "SELECT phone FROM customers WHERE phone ='$RECEIVED_PHONE'")
+      # see if number exists
+      CUSTOMER_PHONE=$($PSQL "SELECT phone FROM customers WHERE phone ='$RECEIVED_PHONE'")
 
-    if [[ -z $CUSTOMER_PHONE ]]
-    then
-      echo "Sorry, we do not have a record of you, What is your name?"
-      read CUSTOMER_NAME
+      if [[ -z $CUSTOMER_PHONE ]]
+      then
+        echo -e "\nSorry, we do not have a record of you, what is your name?"
+        read CUSTOMER_NAME
 
-      # insert customer_name, customer_phone
-      INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$RECEIVED_PHONE')")
-      # set CUSTOMER_PHONE
-    fi
+        # insert customer_name, customer_phone
+        INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$RECEIVED_PHONE')")
+        # set CUSTOMER_PHONE
+      fi
 
-    # Retrieve customer_id, name and phone from select statement
-    read -r CUSTOMER_ID BAR CUSTOMER_NAME BAR CUSTOMER_PHONE <<<$(echo $($PSQL "SELECT customer_id, name, phone FROM customers WHERE phone='$RECEIVED_PHONE'"))
-    echo "$CUSTOMER_ID $CUSTOMER_NAME $CUSTOMER_PHONE"
-    
-    # 
+      # Retrieve customer_id, name and phone from select statement
+      read -r CUSTOMER_ID BAR CUSTOMER_NAME BAR CUSTOMER_PHONE <<<$(echo $($PSQL "SELECT customer_id, name, phone FROM customers WHERE phone='$RECEIVED_PHONE'"))
+      # info that exists in customers
+      # echo "$CUSTOMER_ID $CUSTOMER_NAME $CUSTOMER_PHONE"
+      
+      # ask what time they would like to book their $SERVICE_NAME
+      echo -e "What time would you like to book your $SERVICE_NAME, $CUSTOMER_NAME?"
+      read SERVICE_TIME
+
+      # insert
+      APPOINTMENT_RESPONSE=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES('$CUSTOMER_ID', '$SERVICE_ID', '$SERVICE_TIME')")
+      # echo "$APPOINTMENT_RESPONSE"
+      echo "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+
   fi
 }
-
-
-
-
-
 
 
 SERVICE_MENU
